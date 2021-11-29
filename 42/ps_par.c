2be24cafeb30cf232c70f09fc314736600287c96
Pablo Otero <potero91@gmail.com>
2021-11-29 12:05:48 +0100

    short list

--- 42/ps_par.c
+++ 42/ps_par.c
@@ -6,7 +6,7 @@
 /*   By: potero <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 11:42:05 by potero            #+#    #+#             */
-/*   Updated: 2021/11/26 12:32:41 by potero-d         ###   ########.fr       */
+/*   Updated: 2021/11/29 11:59:16 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ void	ps_par(t_column **column_a, t_column **column_b)
 	t_column	*aux;
 
 	ps_limit_min(*column_a);
+	ps_limit_max(*column_a);
 	aux = *column_b;
 	if ((*column_a)->min == 1)
 		ps_push(column_a, column_b, 'b');
+	else if ((*column_a)->max == 1)
+		ps_rotate(column_a, 'a');
 	else if ((*column_a)->num > ((*column_a)->next)->num)
 		ps_swap(column_a, 'a');
 	else
